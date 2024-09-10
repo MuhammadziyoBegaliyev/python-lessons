2944c26c82c366b8a32e2b58b2ab4ff4b66f82b9
MUhammadziyo <muhammadziyobegaliyev872@gmail.com>
2024-09-10 11:55:13 +0500

    small changees

---
+++ homework001.py
@@ -0,0 +1,27 @@
+#a=10
+#print(a**2,"Onning kvatrti  " ,a**3," o`nning kubi ","")
+#age = int(input("Tugulgan yilingizni kiriting :  "))
+#age1 = 2024-(age)
+#print("Siz shu yoshda ekansiz  "+str(age1))
+#a= int(input("Bir son kiriting : "))
+#b= int(input("ikkinchi sonni kiriting: "))
+#print(a+b,"Shu sonlar yigindisi ",a-b,"Shu sonlar ayirmasi " , a*b, "shu sonlar kopaymasi ", a/b ,"shu sonlar bolinmasi ", a//b ,"shu sonlar bolinmasi ")
+#names = ["Doston","ozodbek","Muhammadali ","Dilshodbek "]
+#name_d = name.pop(0)
+#print("Salom  "+name_d+" Bugun qizing bilan uchrashdingmi ? chimo ")
+#print("Nma gap nma nm gap ukam " + name_2)
+#sonlar = [15,25,5,10.5,-10,24.2,2]
+
+#sonlar.append(26)
+#sonlar[1]= 4 
+#sonlar[3] = sonlar[3]+0.5
+#sonlar[0] = sonlar[1]+sonlar[2]
+#print(sonlar)
+friends = []
+friends.append("doston")
+friends.append("muhammadali")
+friends.append('ozodbek')
+friends.append('dilshodbek')
+friends = friends.pop(3)
+friends.insert(0, 'Dilshodbek')
+print("ertaga nechchida korishamz  ",friends)
