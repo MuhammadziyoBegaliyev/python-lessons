8be5a86108f8967b6dc57dc7f7157cbc224af7f5
MUhammadziyo <muhammadziyobegaliyev872@gmail.com>
2024-09-11 16:45:09 +0500

    git@github.com:MuhammadziyoBegaliyev/homework002.py.git

--- homework001.py
+++ homework001.py
@@ -18,10 +18,41 @@
 #sonlar[0] = sonlar[1]+sonlar[2]
 #print(sonlar)
 friends = []
-friends.append("doston")
-friends.append("muhammadali")
-friends.append('ozodbek')
-friends.append('dilshodbek')
-friends = friends.pop(3)
-friends.insert(0, 'Dilshodbek')
-print("ertaga nechchida korishamz  ",friends)
+# friends.append("doston")
+# friends.append("muhammadali")
+# friends.append('ozodbek')
+# friends.append('dilshodbek')
+# friends.pop(3)
+# friends.insert(0, 'Dilshodbek')
+# print("ertaga nechchida korishamz  ",friends)
+
+
+# kocha = "San`at"
+# mahalla = 'Saxovat'
+# tuman = "Baliqchi "
+# viloyat = "Andijon "
+# print(f"{kocha},kochasi {mahalla}, Mahallasi {tuman }, Tumani{viloyat}, Viloyati")
+
+
+# kocha = input("Sizning kochangiz")
+# mahalla = input("Sizning mahallangiz ")
+# tuman = input("Sizning tuman ")
+# viloyat = input("Sizning viloyatingiz ")
+# print(f"Sizning kochangiz>>> {kocha} Sizning mahallangiz>>> {mahalla} sizning tumanningiz {tuman }   sizning viloyatingiz {viloyat} ")
+
+
+
+# kocha = input("Sizning kochangiz")
+# mahalla = input("Sizning mahallangiz ")
+# tuman = input("Sizning tuman ")
+# viloyat = input("Sizning viloyatingiz ")
+# print(kocha,"Kochangiz \n", mahalla , "mallangiz \n", tuman ,"Tumaningiz\n", viloyat,"viloyatingiz")
+
+# kocha = input("Sizning kochangiz: ")
+# mahalla = input("Sizning mahallangiz ")
+# tuman = input("Sizning tuman ")
+# viloyat = input("Sizning viloyatingiz ")
+# print(f"kochangiz{kocha.title()} ,  mahallangiz {mahalla.upper()}, tumaningiz {tuman.lower()}, viloyatingiz {viloyat.capitalize()}")
+
+
+
---
+++ homework002.py
@@ -0,0 +1,64 @@
+# davlatlar = ['ozbekiston',"tojikiston",'turkmaniston','olimbek']
+# print("Davlatlar soni>>>",len(davlatlar))
+# #davlatlar.sort()
+# print(sorted(davlatlar))
+# print(davlatlar)
+# davlatlar.sort(reverse=True)
+# print(davlatlar)
+# davlatlar.sort()
+# print(davlatlar)
+# davlatlar.sort(reverse=True)
+# print(davlatlar)
+
+
+
+
+
+# sonlar = list(range(120,1200,2))
+# print(sonlar)
+# yigindi =sum(sonlar)
+# print(yigindi,"juft sonlar yigindisi")
+# max = max(sonlar)
+# min = min(sonlar)
+# ayirma = max -min 
+# print("maxsimum qiymat=",max,"  ","minumum qiymat=", min ,"   ", ayirma, "=max - min")
+# print("Elementlar soni = ",len(sonlar))
+# print(sonlar[:21])
+# print(sonlar[260:271])
+# print(sonlar[520:])
+
+
+
+# taomlar =()
+# taomlar = list(taomlar)
+# taomlar.append('mastova')
+# taomlar.append('osh')
+# taomlar.append('shorva')
+# taomlar.append('norin')
+# taomlar.append('xonim')
+# print(taomlar,"=taomlar royhati")
+# nonushta = taomlar[:]
+# nonushta.append("non")
+# nonushta.append('qaymoq')
+# print("Milliy taomlar", taomlar,"  ", "Nonushta >>>", nonushta )
+# nonushta[0]= "non va qaymoq"
+# nonushta = tuple(nonushta)
+# print(nonushta ,"----Taomlar royhati ")
+
+
+# ismlar = ['vali','bobur', 'nodir','javlon','kamron','husan']
+# for ism in ismlar :
+#     print(f"Ishlar qale ---", ism )
+# print("Elementlar soni :",len(ism))
+
+
+# sonlar = list(range(11,100,2))
+# for son in sonlar:
+#     print(f"Sonlarning uchunchi darajasi {son**3}ga teng ")
+
+
+
+kinolar = []
+for kino in kinolar :
+    print(f"yoqtirgan kinolaringiz 1={kinolar} ,\\n, yoqtirgan kinolaringiz 2={kinolar} ,\\n, yoqtirgan kinolaringiz 3={kinolar}")
+
