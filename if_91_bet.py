07a929947e6fcf19755b3c82c4880ac7cd25e2ed
MUhammadziyo <muhammadziyobegaliyev872@gmail.com>
2024-09-12 16:43:08 +0500

    kabisa yili

--- if_91_bet.py
+++ if_91_bet.py
@@ -1,1 +1,11 @@
-son = int(input("Juft son kirirting ="))
+# 1chi vazifa 
+
+# son = int(input("Juft son kirirting ="))
+# if son%2 == 0 :
+#     print(f"Raxmat siz juft son kiritingiz !   {son}")
+# else :
+#     print("iltimos boshqatdan son kiriting ")
+
+
+
+#
---
+++ kabisa_yili.py
@@ -0,0 +1,5 @@
+yil = int(input("Istalgan yilingizni kiriting >>>"))
+if yil % 4 or yil % 100 :
+    print(f"bu yil kabisa yilidir {yil}")
+else :
+    print("bu yil kabisa yili emas! ")
