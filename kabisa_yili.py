740697140df1404a1669af51a4276a0e991341eb
MUhammadziyo <muhammadziyobegaliyev872@gmail.com>
2024-09-12 16:45:04 +0500

    kabisa yili

--- kabisa_yili.py
+++ kabisa_yili.py
@@ -1,5 +1,5 @@
 yil = int(input("Istalgan yilingizni kiriting >>>"))
-if yil % 4 or yil % 100 :
+if yil % 4 == 0 or yil % 100 == 0 :
     print(f"bu yil kabisa yilidir {yil}")
 else :
     print("bu yil kabisa yili emas! ")
