47410deab4692c36a9814dd76d10f23a6cf45d39
MUhammadziyo <muhammadziyobegaliyev872@gmail.com>
2024-09-13 17:11:26 +0500

    wonderfull

---
+++ boolean_task.py
@@ -0,0 +1,9 @@
+namber = int(input("Marhamat ="))
+word = str(input("Marhamat ="))
+# namber >=0 = True
+# namber %2  = True
+'x'in word 
+if namber % 2 != 0   and namber>=0 :#and {'x' in namber }:
+    print("Shart bajarildi!")
+else :
+    print("Shart bajarilmadi !")
