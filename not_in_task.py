4cb494ef7f2b026e7a49642d5fbdafc2653da8ff
MUhammadziyo <muhammadziyobegaliyev872@gmail.com>
2024-09-13 16:27:53 +0500

    not in operatori

---
+++ not_in_task.py
@@ -0,0 +1,7 @@
+soz = str(input("Istalga soz kiriritng >>>"))
+sozlar = ["salom","hi","hello"]
+if soz.lower() not in sozlar :
+    print(f"Yangi soz kiriritildi {soz}")
+else :
+    print("Yangi soz kiritilmadi")
+          
