b9dd8759d6aaad1f898fbd336dd3fa20847a304a
MUhammadziyo <muhammadziyobegaliyev872@gmail.com>
2024-09-14 12:26:06 +0500

    dictionary

---
+++   dictionary_task.py
@@ -0,0 +1,39 @@
+#  1-task
+
+
+# dad={'name':'braun','age':'1975','village':'Alimbk','address':'Sanat stret'}
+# mam={'name':'anna','age':'1978','village':'Alimbk','address':'Sanat stret'}
+# brather={'name':'john','age':'2000','village':'Alimbk','address':'Sanat stret'}
+# print(f"Father name ={dad['name'].title()},\
+#       Father age = {dad['age']},\
+#       Father village = {dad['village'].title()},\
+#       Father address = {dad['address'].title()},\
+#       Mum name ={mam['name'].title()},\
+#       Mum age = {mam['age'].title()},\
+#       Mum village = {mam['village'].title},\
+#       Mum address = {mam['address'].title()},\
+#       Brather name ={brather['name'].title()},\
+#       Brather age = {brather['age']},\
+#       brther village = {brather['village'].title()},\
+#       Brather address = {brather['address'].title()}")
+
+#  2-task
+# famile_bar={'father':'osh','Mather':'manti','Brather':'HotDog','Sister':'Sushi'}
+# print(f"Father favorite food is {famile_bar['father'].title()},\
+#       mather favorite food is {famile_bar['Mather'].title()},\
+#       brather favorite food is {famile_bar['Brather'].title()},\
+#       sister favorite food is {famile_bar['Sister'].title()}")
+
+#  3- task
+# python = {'integer':'butun sonlar ','float':'onli kasr sonlar','string':'matnlar bilan ishledi ','else':'yoki','if':'agar','for':'uchun ','and':'va','or':'yoki','upper':'hamma harfni kattalashtiradi','title':'birinchi sozni kattalashtiradi '}
+# print(len(python))
+# print(python)
+
+# 4 -task
+word = str(input("Enter word ="))
+dictionary ={'apple':'olma','banana':'banan','apricot':'shaftoli'}
+tarjima = dictionary.get(word)
+if tarjima == None :
+    print("don\`t have this word")
+else :
+    print(f"{word.title()}sozi {tarjima}qilinadi")
--- mistacess.py
+++ mistacess.py
@@ -30,18 +30,51 @@
 
 # 4- error
 
-mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
-               'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+# mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+#                'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
 
 
-for n in range(5):
-    mahsulotlar.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+# for n in range(5):
+#     mahsulotlar.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
 
-if mahsulotlar :
-    for mahsulot in mahsulotlar:
-        if mahsulot in mahsulotlar:
-            print(f"Do'konimizda {mahsulot} bor")
-        else:
-            print(f"Do'konimizda {mahsulot} yo'q")
-else: 
-    print("Savatingiz bo'sh")            
+# if mahsulotlar :
+#     for mahsulot in mahsulotlar:
+#         if mahsulot in mahsulotlar:
+#             print(f"Do'konimizda {mahsulot} bor")
+#         else:
+#             print(f"Do'konimizda {mahsulot} yo'q")
+# else: 
+#     print("Savatingiz bo'sh")            
+
+# 5- error
+# mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+#                'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+
+
+# savat = []
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+
+# bor_mahsulotlar = []
+# mavjud_emas = []
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         bor_mahsulotlar.append(mahsulotlar)
+#     else:
+#         mavjud_emas.append(mahsulotlar)
+
+# if mavjud_emas:
+#   print("Do'konimizda quyidagi mahsulotlar yo'q:")
+# for mahsulot in mavjud_emas:
+#   print(mahsulot)
+# else:
+#   print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+#  6-error
+users = ['alisher1983','aziza','yasina' 'umar']
+
+login = input("Yangi login tanlang:")
+
+if login in users:
+    print('Login band, yangi login tanalng!')
+else:
+    print("Xush kelibsiz!")
