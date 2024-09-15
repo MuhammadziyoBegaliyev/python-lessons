f91ab01d6518f15db72de13945ba6a0ff9f1f228
MUhammadziyo <muhammadziyobegaliyev872@gmail.com>
2024-09-15 14:25:26 +0500

    wow

---
+++ Nesting_task.py
@@ -0,0 +1,122 @@
+# people_0 = {
+#     'Alisher Navoiy':['1441-yil tugulgan ','Gazal mulkining sultoni','eng kop so`zdan fodalangan shoir'],
+#     'Elon musk':['1971- yil tugulgan','Muhandis and ixtirochi ','Twitter egasi'],
+#     'Bill Gates':['1955- yil tugalgan','Dasturchi and yozuvchi','Microsoftga asos solgan ']
+#     }
+# people_1 ={
+#     'Stave Jobs': ['1955-yilda tugulgan ','Yaxshi odam ','Men bu odamga malades diman'],
+#     'men ':['Muhtasham Dasturchi','1-kurs talabasi','TATU ga kirdim']
+# }
+# people =[people_0 , people_1]
+
+
+
+
+
+
+
+
+#  1- task
+# people_0 ={
+#     'who':'Alisher Navoiy','Tugulgan_yil':'1441',
+#     'buyukligi':'gazal mulkini sultoni','daraja':'eng ko`p so`z ishlatgan'
+# }
+# people_1 ={
+#     'who':'Elon Musk ','Tugulgan_yil':'1971',
+#     'buyukligi':'gazal Muhandis and ixtirochi sultoni','daraja':'twitter egasi'
+# }
+# people_2 ={
+#     'who':'Bill gates','Tugulgan_yil':'1955',
+#     'buyukligi':'Dasturchi va yozuvchi','daraja':'microsoftga asos solgan',
+# }
+# people_3 ={
+#     'who':'Stave Jobs','Tugulgan_yil':'1955',
+#     'buyukligi':'Yaxshi odam','daraja':'Men bu odamga malades diman '
+# }
+# people_4 ={
+#     'who':'MR_Begaliyev','Tugulgan_yil':'2006',
+#     'buyukligi':'Muhtasham Dasturchi ','daraja':'Tatuda 1-kurs '
+# }
+# people=[people_0,people_1,people_2,people_3,people_4]
+# for man in people:
+#     # print(f"{man['who'].title()},"
+#     #       f"{man['Tugulgan_yil'].title()},---tugulgan yili"
+#     #       f"{man['buyukligi'].title()}---Mashxurligi"
+#     #       f"{man['daraja'].title()},---darajasi "
+#     #       )
+    
+
+
+
+
+
+
+
+# 2- task
+# people_0 ={
+#     'who':'Alisher Navoiy','Tugulgan_yil':'1441',
+#     'buyukligi':'gazal mulkini sultoni','daraja':'eng ko`p so`z ishlatgan',
+#     'asar':'molhona'
+# }
+# people_1 ={
+#     'who':'Elon Musk ','Tugulgan_yil':'1971',
+#     'buyukligi':'gazal Muhandis and ixtirochi sultoni','daraja':'twitter egasi',
+#     'asar':'odam bo`lish qiyin'
+# }
+# people_2 ={
+#     'who':'Bill gates','Tugulgan_yil':'1955',
+#     'buyukligi':'Dasturchi va yozuvchi','daraja':'microsoftga asos solgan',
+#     'asar':'Ikki eshik orasi'
+# }
+# people_3 ={
+#     'who':'Stave Jobs','Tugulgan_yil':'1955',
+#     'buyukligi':'Yaxshi odam','daraja':'Men bu odamga malades diman ',
+#     'asar':'sariq devni minib'
+# }
+# people_4 ={
+#     'who':'MR_Begaliyev','Tugulgan_yil':'2006',
+#     'buyukligi':'Muhtasham Dasturchi ','daraja':'Tatuda 1-kurs ',
+#     'asar':'kema'
+# }
+# people=[people_0,people_1,people_2,people_3,people_4]
+# for new in people:
+#     print(f"{new['who'].title()}---Muallif,   "
+#           f"{new['asar'].title()}---Asari "
+#           )
+
+
+
+
+
+# 3-task
+# friend = {}
+
+
+# film = {
+#     'doston': input(f"{1}--- enter favorite carton and film")
+# }    
+    
+# print(film.values())
+# film_0 = {
+#     'asal': input(f"{1}--- enter favorite carton and film")
+# }    
+    
+# print(film.values())
+# film_1 = {
+#     'sarvar': input(f"{1}--- enter favorite carton and film")
+# }  
+# friend = [film,film_0,film_1]
+
+#
+# print(film_all.items)
+
+
+film_12= {
+    'doston':{input(f"{1}--- enter favorite carton and film")},
+    'ozodbek':{input(f"{1}--- enter favorite carton and film")    },
+    'anvar':{input(f"{1}--- enter favorite carton and film")}
+}
+
+
+print(film_12.items())
+   
--- dictionar_task_001.py
+++ dictionar_task_001.py
@@ -1,12 +1,16 @@
-city = {
-    'andijon':15_000,
-    'namangan':20_000,
-    'buxoro':17_000,
-    'samarqand':18_000,
-    'navoiy':30_000
-    }
-city['qashqadaryo']=16_000
-print(f"shaxarlar royhati {city.keys()}")
-city['andijon']=25_000
-print(city)
-print(city['buxoro'])
+# city = {
+#     'andijon':15_000,
+#     'namangan':20_000,
+#     'buxoro':17_000,
+#     'samarqand':18_000,
+#     'navoiy':30_000
+#     }
+# city['qashqadaryo']=16_000
+# print(f"shaxarlar royhati {city.keys()}")
+# city['andijon']=25_000
+# print(city)
+# print(city['buxoro'])
+
+
+
+#   
