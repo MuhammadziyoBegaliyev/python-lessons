e6b5ec2d5ece367ed737785f6c26f278d422d278
MUhammadziyo <muhammadziyobegaliyev872@gmail.com>
2024-09-21 13:06:19 +0500

    lambda

---
+++ function_lombda_task.py
@@ -0,0 +1,3 @@
+number = int(input("Istalgan son kiriting ="))
+get_num = lambda number, n : number * n 
+print(get_num(number,10))
--- function_nambers_task.py
+++ function_nambers_task.py
@@ -1,11 +1,10 @@
-def nambres(namber_1,namber_2):
-    """Sonlar ro'yxatidagi eng katta sonni topuvchi funksiya"""
-    namber_1 = int(input("Enter namber one ="))
-    namber_2 =int(input("enter namber two ="))
-    if namber_1>namber_2 :
-        print(namber_1)
-    else :
-        print(namber_2)
-        return namber_2
-nambres(5,10)
-print(nambres)
+
+numbers = [10,15,20,45,62,42,21,32]
+
+def get_max(numbers):
+    result = max(numbers)
+    print(result)
+    
+    return result
+
+get_max(numbers)
