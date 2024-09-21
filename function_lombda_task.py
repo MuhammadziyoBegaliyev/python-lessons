e6ab7b08ecf455b384276e2ce389aedd73acadd8
MUhammadziyo <muhammadziyobegaliyev872@gmail.com>
2024-09-21 13:12:26 +0500

    lambda task2

--- function_lombda_task.py
+++ function_lombda_task.py
@@ -1,3 +1,11 @@
-number = int(input("Istalgan son kiriting ="))
-get_num = lambda number, n : number * n 
-print(get_num(number,10))
+# number = int(input("Istalgan son kiriting ="))
+# get_num = lambda number, n : number * n 
+# print(get_num(number,10))
+
+
+#  task two 
+""" Ikki son qabul qilib ularning yigindisini qaytaruvchi lambda function"""
+number = int(input("Istalgan birinchi son kiriting ="))
+number_2 = int(input("Istalgan ikkinchi sonni kiriting ="))
+get_num2 = lambda number , number_2 : number + number_2 
+print(get_num2(number,number_2))
