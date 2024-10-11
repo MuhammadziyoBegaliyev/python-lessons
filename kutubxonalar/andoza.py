e5614c92765033a8c60c7727f982dfd8784d0a76
MuhammadziyoBegaliyev <git@github.com:MuhammadziyoBegaliyev/python-lessons.git>
2024-10-11 17:06:41 +0500

    kutubxona

--- kutubxonalar/andoza.py
+++ kutubxonalar/andoza.py
@@ -1,9 +1,21 @@
 import re
-nomer = input("Enter you tel nomer :")
-nomer += ("9xonali son , +998dan boshlanishi shart emas ")
-andoza = "^N.......s$" 
 
-try:
-    print(re.match(andoza.nomer))
-except AttributeError:
-    print("Xato kiritilmoqda !")
+
+def validate_phone_number(phone_number):
+    """
+    Validate a phone number in the format 998(7-9)XXXXXXXX.
+    """
+    regex = r"^998[7-9][0-9][0-9]{3}[0-9]{2}[0-9]{2}$"
+
+    match = re.match(regex, phone_number)
+
+    if match:
+        print(f"The phone number '{phone_number}' is valid.")
+        return True
+    else:
+        print(f"The phone number '{phone_number}' is invalid.")
+        return False
+
+
+phone_number_input = input("Please enter a phone number: ")
+validate_phone_number(phone_number_input)
